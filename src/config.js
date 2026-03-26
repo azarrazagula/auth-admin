5e6a369612097b33ceffe642dfd07c88370fa372
azarrazagula <azarrazagula@gmail.com>
2026-03-26 16:11:45 +0530

    fix: admin add foods to be shown auth-frontend page

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://auth-backend-3-4m2m.onrender.com';
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
+const API_BASE_URL = isLocal
+  ? "http://localhost:5001"
+  : process.env.REACT_APP_API_URL || "https://auth-backend-3-4m2m.onrender.com";
 
 export default API_BASE_URL;
